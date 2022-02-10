ded26fda97fe36319a5c580e36395d6c1f4db46d
Dominik Schütz <dominik.schuetz@ropa-maschinenbau.de>
2022-02-10 16:58:16 +0100

    allow manual expiration of services

--- implementation/runtime/include/application_impl.hpp
+++ implementation/runtime/include/application_impl.hpp
@@ -47,6 +47,9 @@ public:
     VSOMEIP_EXPORT void start();
     VSOMEIP_EXPORT void stop();
     VSOMEIP_EXPORT void process(int _number);
+    
+    VSOMEIP_EXPORT void expire_services(const service_t service,
+                                      const instance_t instance) override;
 
     VSOMEIP_EXPORT security_mode_e get_security_mode() const;
 
